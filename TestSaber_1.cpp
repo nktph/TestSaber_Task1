4812049d82e2715535062aea23316ba0b6f84dff
nktph <nkt.ph.coop@gmail.com>
2023-01-22 17:57:41 +0300

    Добавьте файлы проекта.

---
+++ TestSaber_1.cpp
@@ -0,0 +1,37 @@
+﻿// TestSaber Task 1
+//
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class ListNode {
+public:
+    ListNode* Prev;
+    ListNode* Next;
+    ListNode* Rand; // произвольный элемент внутри списка
+    string Data;
+};
+
+
+class ListRand {
+public:
+    ListNode* Head;
+    ListNode* Tail;
+    int Count;
+
+    void Serialize(ofstream s)
+    {
+    }
+
+    void Deserialize(ifstream s)
+    {
+    }
+};
+
+
+int main()
+{
+    
+}
